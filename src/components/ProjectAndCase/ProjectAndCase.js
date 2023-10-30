9994b5c8cb81f8278aab3e11c33e444482a923fa
emry24 <em_ryden@outlook.com>
2023-10-30 17:31:03 +0100

    adjustments made to ProjectAndCase.js

--- src/components/ProjectAndCase/ProjectAndCase.js
+++ src/components/ProjectAndCase/ProjectAndCase.js
@@ -9,37 +9,37 @@ import img_article4 from '../../assets/images/article-image-4.png'
 const ProjectAndCase = () => {
     return (
         <div>
-            <section class="project-and-case">
-                <div class="container">
-                    <div class="section-title">
+            <section className="project-and-case">
+                <div className="container">
+                    <div className="section-title">
                         <p>Project & Case Studies</p>
                         <h2>Let's Looks Our Global Projects</h2>
                     </div>
-                    <div class="project-and-cases">
-                        <a class="project" href="#">
+                    <div className="project-and-cases">
+                        <a className="project" href="#">
                             <img src={img_article1} alt="A mans hand reading a business paper" />
                                 <h3>Grow your business</h3>
-                                <div class="readmore">Read More<i class="fa-regular fa-arrow-up-right"></i></div>
+                                <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                         </a>
-                        <a class="project" href="#">
+                        <a className="project" href="#">
                             <img src={img_article2} alt="Pink Apple products" />
                                 <h3>Why your client needs a responsive website</h3>
-                                <div class="readmore">Read More<i class="fa-regular fa-arrow-up-right"></i></div>
+                                <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                         </a>
-                        <a class="project" href="#">
+                        <a className="project" href="#">
                             <img src={img_article3} alt="Desk with supplies" />
                                 <h3>Educate your employees to get better results</h3>
-                                <div class="readmore">Read More<i class="fa-regular fa-arrow-up-right"></i></div>
+                                <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                         </a>
-                        <a class="project" href="#">
+                        <a className="project" href="#">
                             <img src={img_article4} alt="Laptop with Business Intellegence Insights" />
                                 <h3>Business Insights is a important piece of your business</h3>
-                                <div class="readmore">Read More<i class="fa-regular fa-arrow-up-right"></i></div>
+                                <div className="readmore">Read More<i className="fa-regular fa-arrow-up-right"></i></div>
                         </a>
                     </div>
-                    <div class="center-content">
-                        <a class="btn-black" href="projects.html">All Recent Projects<i
-                            class="fa-regular fa-arrow-up-right"></i></a>
+                    <div className="center-content">
+                        <a className="btn-black" href="projects.html">All Recent Projects<i
+                            className="fa-regular fa-arrow-up-right"></i></a>
                     </div>
                 </div>
             </section>
