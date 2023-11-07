6b012874fc96d7436ae95c21e2664a8820d5266d
emry24 <em_ryden@outlook.com>
2023-11-07 23:44:39 +0100

    Started building form with Formik

--- src/components/FormsSection/FormsSection.js
+++ src/components/FormsSection/FormsSection.js
@@ -1,48 +1,71 @@
 import React, { useState } from 'react'
 import './FormsSection.css'
+import { useFormik } from 'formik'
 
 const FormsSection = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
-
   const [errorMessage, setErrorMessage] = useState('')
   
+  const form = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: ''
+    },
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setErrorMessage('')
+    onSubmit: async (values) => {
+      const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
 
-    const textMessage = {name, email, message}
-    const json = JSON.stringify(textMessage)
-
-    const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
-      method: 'post',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: json
-    })
-
-    clearForm()
-
-    switch (result.status) {
-      case 200: 
-        clearForm()
-        alert('Meddelandet har skickats')
-        break
-      case 400:
-          setErrorMessage('Något gick fel. Kontrollera att fälten är korrekt ifyllda.')   
-          break
+      switch (result.status) {
+        case 200:
+          alert('Meddelandet har skickats.')
+          break;
+        case 400:
+          setErrorMessage('Något gick fel. Kontrollera att fälten är korrekt ifyllda.') 
+          break;
+      }
     }
+  })
 
-  }
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   setErrorMessage('')
 
-  const clearForm = () => {
-    setName('')
-    setEmail('')
-    setMessage('')
-  }
+  //   const textMessage = {name, email, message}
+  //   const json = JSON.stringify(textMessage)
+
+  //   const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+  //     method: 'post',
+  //     headers: {
+  //       'content-type': 'application/json'
+  //     },
+  //     body: json
+  //   })
+
+  //   clearForm()
+
+  //   switch (result.status) {
+  //     case 200: 
+  //       clearForm()
+  //       alert('Meddelandet har skickats.')
+  //       break
+  //     case 400:
+  //         setErrorMessage('Något gick fel. Kontrollera att fälten är korrekt ifyllda.')   
+  //         break
+  //   }
+
+  // }
+
+  // const clearForm = () => {
+  //   setName('')
+  //   setEmail('')
+  //   setMessage('')
+  // }
 
 
   return (
@@ -50,11 +73,11 @@ const FormsSection = () => {
     <div className="container">
         <h2>Leave us a message for any information.</h2>
         
-        <form onSubmit={handleSubmit} noValidate>
+        <form onSubmit={form.handleSubmit} noValidate>
             <p className='errorMessage'>{errorMessage}</p>
-            <input type="text" required placeholder="Name*" value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="text" required placeholder="Email*" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <textarea cols="30" rows="10" placeholder="Your Message*" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            <input type="text" required placeholder="Name*" name="name" value={form.values.name} onChange={form.handleChange} />
+            <input type="text" required placeholder="Email*" name="email" value={form.values.email} onChange={form.handleChange} />
+            <textarea cols="30" rows="10" placeholder="Your Message*" name="message" value={form.values.message} onChange={form.handleChange}></textarea>
             <button className="btn-yellow btn-login" type="submit">Send Message<i className="fa-regular fa-arrow-up-right"></i></button>
         </form>
         
